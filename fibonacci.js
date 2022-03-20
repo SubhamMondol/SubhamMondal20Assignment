85d651049b600897e8e8aed38e0e66359fc793ac
SubhamMondol <92972945+SubhamMondol@users.noreply.github.com>
2022-03-20 16:51:22 +0530

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,23 @@
+// Program to display fibonacci number
+function fibonacci(num){
+    if(num<2){
+        return num;
+    }
+        else
+        {
+            return fibonacci(num-1) + fibonacci(num-2);
+        }
+    }
+
+    // Take the input from the user
+    const nTerms =prompt('Enter the number of terms:');
+    if(nTerms<=0)
+    {
+        console.log('Enter a Positive integer');
+    }
+    else{
+        for(let i = 0; i < nTerms; i++)
+        {
+            console.log(fibonacci(i));
+        }
+    }
